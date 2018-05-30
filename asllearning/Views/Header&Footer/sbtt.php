a51b2f8e5af9909bbc306ce93fd6bf9559accca2
nifra514 <nifras.ict>
2018-05-30 09:21:36 +0530

    Edited

--- asllearning/Views/Header&Footer/sbtt.php
+++
@@ -1,4 +0,0 @@
-<div class="scroll-top-wrapper">
-	<span class="glyphicon glyphicon-menu-up"
-		style="font-size: 30px; line-height: 1.5;"></span>
-</div>
--- asllearning/core/Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-// require_once('../Models/mysqldb.php');
-// class Model extends mysqldb{
-	
-// 	public function query($query) {
-// 		return $this->comq($query);
-// 	}
-// }
-?>
