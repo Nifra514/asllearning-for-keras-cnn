e22652746b35ee4c92f1cd40e06282aac2cc4f0f
Nifra514 <nifras.ict@gmail.com>
2018-07-27 11:52:35 +0530

    Edited

--- Core/UserCore.php
+++ Core/UserCore.php
@@ -5,7 +5,7 @@ $sql_cntrl = new control ();
 $logged_user = null;
 
 if(isset($_SERVER['HTTP_X_TOKEN'])) {
-	$res = $sqlCntrl->get_user_from_token($_SERVER['HTTP_X_TOKEN']);
+	$res = $sql_cntrl->get_user_from_token($_SERVER['HTTP_X_TOKEN']);
 	if($res->num_rows > 0){
 		$logged_user = $res->fetch_assoc();
 	}
