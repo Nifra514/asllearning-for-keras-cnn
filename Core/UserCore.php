b6fd3d687ccc3dd24748c803300ad6b29e13dcac
Nifra514 <nifras.ict@gmail.com>
2018-08-01 19:35:46 +0530

    Edited

--- Core/UserCore.php
+++ Core/UserCore.php
@@ -6,7 +6,7 @@ $logged_user = null;
 
 if(isset($_SERVER['HTTP_X_TOKEN'])) {
 	$res = $sql_cntrl->get_user_from_token($_SERVER['HTTP_X_TOKEN']);
-	if($res->num_rows > 0){
-		$logged_user = $res->fetch_assoc();
+	if((mysqli_num_rows($res)) > 0){
+		$logged_user = mysqli_fetch_array($res);
 	}
 }
