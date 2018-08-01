85b95c78ed0e363bdff6451062e95aac9b615ba9
Nifra514 <nifras.ict@gmail.com>
2018-08-01 19:38:29 +0530

    Edited

--- Models/mysqldb.php
+++ Models/mysqldb.php
@@ -21,22 +21,9 @@ class mysqldb {
 	
 	public function comq($sql) {
 		$res = $this->link->query ( $sql );
-// 		if(!$res){
-// 			$error = mysqli_errno($this->link);
-// 			$this->write_log('NULL', 'error', $error, 'none', 1, 'High');
-// 		}
 		return $res;
 	
 	}
-	
-// 	public function comq($sql) {
-// 		return $this->link->query ( $sql ); 
-// 	}
-	
-// 	function write_log($user_id, $type, $data, $action, $status, $risk){
-// 		$encoded_data = json_encode($data);
-// 		$query = "INSERT INTO `log_details` (`u_id`, `type`, `data`, `action`, `status`, `timestamp`, `risk`) VALUES ('$user_id', '$type', '$encoded_data', '$action', '$status', NOW(), '$risk')";
-// 		$this->comq($query);
-// 	}
+
 }
 ?>
