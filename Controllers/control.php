8086c56ef3b42a7324cfa41fc77a4b92acd3a8ac
Nifra514 <nifras.ict@gmail.com>
2018-08-01 19:35:01 +0530

    Edited

--- Controllers/control.php
+++ Controllers/control.php
@@ -5,7 +5,11 @@ class control extends mysqldb {
 		parent::__construct ();
 	}
 	function write_log($user_id, $type, $data, $action, $risk) {
-		$encoded_data = json_encode ( $data );
+		
+		$encoded_data = $data;
+		if(gettype($data) != 'string'){
+			$encoded_data = json_encode($data);
+		}
 		
 		date_default_timezone_set ( 'Asia/Colombo' );
 		$time = date ( 'H:i:s' );
@@ -14,20 +18,24 @@ class control extends mysqldb {
 		$query = "INSERT INTO `log_details` (`u_id`, `type`, `data`, `action`, `status`, `risk`, `date`, `time`) VALUES ('$user_id', '$type', '$encoded_data', '$action', '1', '$risk', '$date','$time')";
 		$this->comq ( $query );
 	}
-	function get_user_from_token($token) {
-		$sql = "SELECT * FROM user_details WHERE token='" . $token . "'";
+	function get_user_from_id($u_id) {
+		$sql = "SELECT * FROM user_details WHERE u_id='$u_id'";
 		return $this->comq ( $sql );
 	}
-	function token_up($token, $uname) {
-		$sql = "UPDATE user_details SET token='$token' WHERE username='$uname'";
+	function get_user_from_token($token) {
+		$sql = "SELECT * FROM user_details WHERE token='$token'";
+		return $this->comq ( $sql );
+	}
+	function token_up($token, $uname , $password) {
+		$sql = "UPDATE user_details SET token='$token' WHERE username='$uname' && password = '$password'";
 		return $this->comq ( $sql );
 	}
 	function exists($uname) {
 		$sql = "SELECT u_id FROM user_details WHERE username = '$uname'";
 		return $this->comq ( $sql );
 	}
-	function pwd_exists($password) {
-		$sql = "SELECT u_id FROM user_details WHERE password = '$password'";
+	function un_pwd_exists($uname,$password) {
+		$sql = "SELECT u_id FROM user_details WHERE username = '$uname' && password = '$password'";
 		return $this->comq ( $sql );
 	}
 	function email_exists($email) {
@@ -38,7 +46,7 @@ class control extends mysqldb {
 		return '<ul><li>' . implode ( '</li><li>', $errors ) . '</li></ul>';
 	}
 	public function reg_user($name, $email, $phone, $uname, $password1) {
-		$sql = "INSERT INTO `user_details`(`name`, `email_id`,`tp_no`, `username`, `password`, `user_type`,`date`)
+		$sql = "INSERT INTO `user_details`(`u_name`, `email_id`,`tp_no`, `username`, `password`, `user_type`,`date`)
 		VALUES ('$name','$email','$phone','$uname','$password1', 'user',NOW())";
 		
 		return $this->comq ( $sql );
